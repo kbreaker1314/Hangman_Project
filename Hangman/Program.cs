78576f06cb2a847210e68bb231d31b593e2628e6
Bao Tran <77811454+kbreaker1314@users.noreply.github.com>
2021-02-04 09:51:09 -0800

    Intro and board generation

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Hangman
 {
@@ -7,18 +8,56 @@ namespace Hangman
     //Making an array to store list of words for user to guess - COMPLETE
     //Making an array of characters and split the words into chars
 
+    //POSSIBLE function:
+    //Having user add a word to guess
+    //HINTS?
     class Program
     {
-        static string ListOfWords()
+        static string theRealWord()
         {
+            //Going through the list of words and randomize one word for the user to guess
             var generator = new Random();
             string[] arr = { "awesome", "lovely" };
-            int randomNumber = generator.Next(0, arr.Length - 1);
-            return arr[randomNumber];
+            int theActualWord = generator.Next(0, arr.Length - 1);
+            return arr[theActualWord];
         }
+        static void Board(string passingWord)
+        {
+            //passing the word from Main and convert into char type and print the spaces required.
+            char[] character = passingWord.ToCharArray();
+            //unnesscary for now
+            char[] fakeWord = character;
+            Console.WriteLine(character);
+            for (int i = 0; i < fakeWord.Length; i++)
+            {
+                Console.Write("_ ");
+            }
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine(ListOfWords());
+            //bool gameRun = true;
+            /// INTRO TO THE GAME
+            Console.WriteLine("Welcome to Hangman\n\n\n");
+            Console.WriteLine("All you have to do is guess the world -- GOODLUCK\n\n");
+            /// Making a copy of the word and pass it to Board method
+            string copyRealWord = theRealWord();
+            Board(copyRealWord);
+            //do
+            //{
+            //    Console.WriteLine("Guess a letter: ");
+            //    char input = Console.ReadLine()[0];
+            //    Console.WriteLine(input);
+            //    
+            //    
+            //
+            //
+            //
+            //    gameRun = false;
+            //} while (gameRun);
+            //
+            //
+
         }
     }
 }
