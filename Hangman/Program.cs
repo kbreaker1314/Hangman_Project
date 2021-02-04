2122ca4a7b201088d7f29afacb3d1e640f31fc9d
Bao Tran <77811454+kbreaker1314@users.noreply.github.com>
2021-02-04 07:57:00 -0800

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Hangman
+{
+
+    //Objectives:
+    //Making an array to store list of words for user to guess - COMPLETE
+    //Making an array of characters and split the words into chars
+
+    class Program
+    {
+        static string ListOfWords()
+        {
+            var generator = new Random();
+            string[] arr = { "awesome", "lovely" };
+            int randomNumber = generator.Next(0, arr.Length - 1);
+            return arr[randomNumber];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ListOfWords());
+        }
+    }
+}
